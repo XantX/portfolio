425388be9a919e188929a0ba1d49559aad97f9b7
XantX <hp_xantx@hotmail.com>
2022-10-05 01:37:27 -0500

    Feature: Agregado componente de banner con titulo y subctitulo Agregado colores de gruvbox-dark y gruvbox-light

---
+++ src/components/banner/banner.js
@@ -0,0 +1,12 @@
+import "./banner.css"
+
+function Banner() {
+  return(
+    <div className="banner-container">
+      <h1 className="title">My Porfolio</h1>
+      <h1 className="subtitle">Hi mi name is Sebastian</h1>
+    </div>
+  );
+}
+
+export default Banner;
