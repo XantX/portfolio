35c7d9c1a8b55ca3093590025e2ddcf05b846e63
XantX <hp_xantx@hotmail.com>
2022-10-07 03:08:42 -0500

    Agregada la barra de navegacion para la pagina

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import './App.css';
 import './shared/gruvbox-dark.css'
 import Banner from './components/banner/banner.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
   return (
     <div className="App">
-      <Banner/>
+      <nav className="navbar navbar-expand-lg navbar-dark background">
+        <a className="gruv-nav-brand navbar-brand" href="https:\\google.com.pe">XantX</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item active">
+              <a className="gruv-nav-link nav-link" href="https:\\google.com.pe">About me</a>
+            </li>
+            <li className="nav-item">
+              <a className="gruv-nav-link nav-link" href="https:\\google.com.pe">Experience</a>
+            </li>
+            <li className="nav-item">
+              <a className="gruv-nav-link nav-link" href="https:\\google.com.pe">Projects</a>
+            </li>
+          </ul>
+          <div className='icons-container me-4'>
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a target={'_blank'} className="gruv-nav-link nav-link" href="https:\\github.com\XantX">
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+              <li className="nav-item">
+                <a target={'_blank'} className="gruv-nav-link nav-link" href="https:\\www.linkedin.com\in\sebastian-diaz-torres-43058a161">
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <Banner />
     </div>
   );
 }
--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -2,7 +2,7 @@ import "./banner.css"
 
 function Banner() {
   return(
-    <div className="banner-container">
+    <div className="banner-container text-wrap">
       <h1 className="title">My Porfolio</h1>
       <p className="subtitle">Hi mi name is <strong>Sebastian</strong> &#127839;</p>
     </div>
