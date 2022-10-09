c5f42f49b9eb62dfeae33c963b62dd645d1aee68
XantX <hp_xantx@hotmail.com>
2022-10-09 17:15:00 -0500

    Eliminado el uso de autorization en la function  para crear el objeto de axios

--- src/components/github-card/github-services/axios-api.js
+++ src/components/github-card/github-services/axios-api.js
@@ -1,9 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
   baseURL: `https://api.github.com/`,
-  headers: { 'Authorization': `bearer ${process.env.REACT_APP_API_KEY}` }
-})
-
+});
 
 export default api;
