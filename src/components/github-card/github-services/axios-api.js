cce7362367319decb8785ceb5a06091c3fba8355
XantX <hp_xantx@hotmail.com>
2022-10-09 00:19:41 -0500

    pruebas de variables de entorno

--- src/components/github-card/github-services/axios-api.js
+++ src/components/github-card/github-services/axios-api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 const api_key = process.env.REACT_APP_API_KEY
+const token_test = process.env.REACT_APP_TEST_TOKEN
+console.log(token_test)
 const api = axios.create({
   baseURL: `https://api.github.com/`,
   headers: { 'Authorization': `bearer ${api_key}` }
