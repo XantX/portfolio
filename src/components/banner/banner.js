997edfb68424dc827a1e03327113f3e2e8a2aceb
XantX <hp_xantx@hotmail.com>
2022-10-08 23:33:51 -0500

    Configuracino de actions otra vez

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -7,7 +7,7 @@ function Banner() {
       <div className="row">
         <div className="col left">
           <h1 className="title">My Porfolio</h1>
-          <p className="subtitle">Hi mi name is <strong className="green">Sebastian</strong> aka. <strong className="green">XantX</strong></p>
+          <p className="subtitle">HI mi name is <strong className="green">Sebastian</strong> aka. <strong className="green">XantX</strong></p>
         </div>
         <div className="col">
           <UserCard></UserCard>
