008d1637aba0c7c555e295822a428af7adc1d829
XantX <hp_xantx@hotmail.com>
2022-10-09 00:27:54 -0500

    rebuild

--- src/components/github-card/github-services/axios-api.js
+++ src/components/github-card/github-services/axios-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api_key = process.env.REACT_APP_API_KEY
-const token_test = process.env.REACT_APP_TEST_TOKEN
+const token_test = process.env.REACT_APP_TOKEN_TEST
 console.log(token_test)
 const api = axios.create({
   baseURL: `https://api.github.com/`,
