8fcf092276ad4473cf30c114305aa7bd7b4eb752
XantX <hp_xantx@hotmail.com>
2022-10-09 15:44:45 -0500

    config router

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './shared/font-firacode.css'
 import App from './App';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
     <Routes>
       <Route path='/' element={<App />} />
-      <Route path='*' element={<App />} />
+      <Route path='*' element={ <Navigate to="/"/> } />
     </Routes>
   </HashRouter>
 );
