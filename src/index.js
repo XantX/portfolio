d79fbba9bf03ce2e284a372c1cdffbcdd886badf
XantX <hp_xantx@hotmail.com>
2023-02-15 22:10:06 -0500

    Correciones en no used i18n

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './shared/font-firacode.css'
 import App from './App';
-// eslint-disable-next-line no-unused-vars
-import i18n from './traslations/traslate-config';
+import './traslations/traslate-config';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
